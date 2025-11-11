2d711a84f76ff0d90fadb2cb25ebb4cc58539b9c
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-11 03:30:10 -0500

    Grandma Arms

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ function createGrandma() {
     const headMaterial = new THREE.MeshPhongMaterial({ color: 0xFFE4B5 }); // Skin color
     const head = new THREE.Mesh(headGeometry, headMaterial);
     head.position.y = 2.5;
-    
+
      // Hair (grey bun)
     const hairGeometry = new THREE.SphereGeometry(0.4);
     const hairMaterial = new THREE.MeshPhongMaterial({ color: 0x808080 }); // Grey hair
     const hair = new THREE.Mesh(hairGeometry, hairMaterial);
     hair.position.y = 2.7;
     hair.position.z = -0.2;
+     
+    // Arms
+    const armGeometry = new THREE.CylinderGeometry(0.2, 0.2, 1.5);
+    const armMaterial = new THREE.MeshPhongMaterial({ color: 0x800080 });
+    
