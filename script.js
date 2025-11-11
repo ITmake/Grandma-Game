ae24cb3e124c355c7cfbd4ef894444b0397d9ba9
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-11 03:26:04 -0500

    Grandma Body

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let score = 0;
 function createGrandma() {
     // Create grandma group
     grandma = new THREE.Group();
+        
+    // Body (dress)
+    const dressGeometry = new THREE.ConeGeometry(1, 2, 8);
+    const dressMaterial = new THREE.MeshPhongMaterial({ color: 0x800080 }); // Purple dress
+    const dress = new THREE.Mesh(dressGeometry, dressMaterial);
+    dress.position.y = 1;
