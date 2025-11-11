525a6d556a1c90add433d0342acfa24ce773acf7
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-11 03:34:56 -0500

    Create Renderer

--- script.js
+++ script.js
@@ -58,3 +58,9 @@ scene.add(grandma);
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 15;
     camera.position.y = 5;
+
+// Create renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('game-container').appendChild(renderer.domElement);
+    
