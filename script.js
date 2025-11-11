d0b0836f362fa274837903a1f5569e4462ae2cd7
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-11 03:55:11 -0500

    Handle Window Re Size

--- script.js
+++ script.js
@@ -232,3 +232,10 @@ function animate() {
 
 init();
 animate();
+
+// Handle window resize
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
