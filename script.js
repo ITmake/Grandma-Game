6fb59ec981011085db0efe1594895a3e9bb1d319
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-11 03:25:32 -0500

    Function "create grandma"

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let scene, camera, renderer, grandma, apples = [], oranges = [], stones = [];
 let score = 0;
+
+function createGrandma() {
+    // Create grandma group
+    grandma = new THREE.Group();
